22b4f458bf7783bb9aa43cbdfa2c54bc146b4b3d
Francesco Baldi <francesco.baldi86@gmail.com>
2019-06-04 16:34:10 +0200

    Update cachematrix.R
    First proper commit. Functions added and tested

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inverse <- NULL   # The inverse matrix is set to null by default
+      set <- function(y) {    # The set function for this object is used to define the matrix
+            x <<- y
+            inverse <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(inverse_input) inverse <<- inverse_input
+      getinverse <- function() inverse
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
 
 
@@ -12,4 +22,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      inverse <- x$getinverse()
+      if(!is.null(inverse)) {
+            message("Existing stored inverse matrix. Now checking if it is correct")
+            return(inverse)
+      }
+      else {
+            data <- x$get()
+            inverse <- solve(data)
+            x$setinverse(inverse)
+      }
+      inverse
 }
